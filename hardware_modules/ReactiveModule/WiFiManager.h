3d24c6bf7b9730b6a751050fb9264fab4bd83e81
fcaanaak <fcanak05@gmail.com>
2025-09-23 21:44:19 -0700

    Began creating WiFiManager class for Notion task 1

---
+++ hardware_modules/ReactiveModule/WiFiManager.h
@@ -0,0 +1,32 @@
+#ifndef WIFI_MANAGER_H
+#define WIFI_MANAGER_H
+
+#include <Preferences.h>
+#include <WiFi.h>
+
+class WiFiManager{
+
+ private:
+
+  const unsigned short autoReconnectSeconds = 10;
+
+  Preferences prefObject;
+
+  void connectToWiFi(const char* ssid, const char* password);
+  String scanStoredNetworks();
+  bool autoReconnect();
+
+  void registerWiFiEvents();
+  void registerWiFiConnected();
+  void resetToSTA();
+  void setupWiFi();
+
+  static void onWiFiReconnect(WiFiEvent_t event, WiFiEventInfo_t info);
+  
+ public:
+  WiFiManager(Preferences passedPrefObject);
+  void setup();
+  
+};
+
+#endif
---
+++ hardware_modules/ReactiveModule/WifiManager.cpp
@@ -0,0 +1,97 @@
+#include "WiFiManager.h"
+#define WIFI_DATABASE "wifiDatabase"
+#define READ true
+#define READ_WRITE false
+
+
+void WiFiManager::resetToSTA(){
+  /**
+     @brief Set the esp32 to STA mode in case it wasn't already
+   */
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+}
+
+void WiFiManager::connectToWiFi(const char* ssid, const char* password){
+   /**
+     @brief Connect to a WiFi network
+     @param ssid: The ssid or name of the WiFi network
+     @param password: The password of the WiFi network 
+   */
+  Serial.print("Connecting to Wifi network with credentials: ");
+  Serial.print(ssid);
+  Serial.print(" ");
+  Serial.println(password);
+
+  
+  // Add logic if we fail to connect
+  WiFi.begin(ssid,password);
+  
+  // Connecting to WiFi takes time, this will wait
+  while (WiFi.status() != WL_CONNECTED){
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println();
+}
+
+String WiFiModule::scanStoredNetworks(){
+
+  prefObject.begin(WIFI_DATABASE,READ);
+
+  int availableNetworks = WiFi.scanNetworks();
+
+  if (availableNetworks > 0) {
+
+    for (int currentNetwork = 0;(currentNetwork < availableNetworks);currentNetwork++){
+      
+      String ssid = WiFi.SSID(currentNetwork);
+      //Serial.println(ssid);// Remove this later
+      
+      if (prefObject.getString(ssid.c_str(),"fail") != "fail"){	
+
+	Serial.print("Found SSID: ");
+	Serial.println(ssid);
+
+	WiFi.scanDelete();
+	prefObject.end();
+        return ssid;
+      }
+     
+    }
+  }
+  
+  prefObject.end();
+  return String();
+
+}
+
+bool WiFiManager::autoReconnect(){
+  /**
+     @brief Automatically reconnect to a previously saved WiFi network
+     if said network is found during scanning
+
+     @return true if a WiFi network was able to be connected
+     to and false otherwise
+   */
+  String storedSSID = scanStoredNetworks();
+ 
+  if (storedSSID != ""){
+    Serial.println(storedSSID);
+    prefObject.begin(WIFI_DATABASE,READ);
+    String password = prefObject.getString(storedSSID.c_str());
+    const char* password_cstring = password.c_str();
+    Serial.print("Password: ");
+    Serial.println(password);
+    Serial.print("C string password: ");
+    Serial.println(password_cstring);
+
+    prefObject.end();
+    connectToWiFi(storedSSID.c_str(),password_cstring);
+    return true;
+  }
+  
+  return false;
+
+}
